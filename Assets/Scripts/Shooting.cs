7e21f37933136146219af17f06403cef51a16330
joaoarthursilva1 <joaoartsilva@gmail.com>
2022-09-21 18:56:20 -0300

    idk

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -13,18 +13,16 @@ public class Shooting : MonoBehaviour
     private GameObject _bullet;
     public float bulletForce = 20f;
     private Rigidbody2D _rb;
-
+    public float shootDelay = 2f;
+    private float _shootDelayCounter;
+    
     private void Start()
-    {
+    {        
     }
 
-    void Update()
+    private void Update()
     {
         _mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Shoot();
-        }
     }
 
     private void Shoot()
@@ -36,8 +34,22 @@ public class Shooting : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 lookDir = _mousePos - new Vector2(_gunPivot.position.x, _gunPivot.position.y);
+        Vector3 position = _gunPivot.position;
+        Vector2 lookDir = _mousePos - new Vector2(position.x, position.y);
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         transform.rotation = Quaternion.Euler(0f, 0f, angle + 90);
+        // Debug.Log(_shootDelayCounter);
+        if (canShoot())
+        {
+                Shoot();
+                // _shootDelayCounter = shootDelay;
+        }
+        if(_shootDelayCounter>0f)
+            _shootDelayCounter -= Time.deltaTime;
+    }
+
+    private bool canShoot()
+    {
+        return Input.GetButton("Fire1"); //&& _shootDelayCounter>0f;
     }
 }
